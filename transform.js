1db1a101e318f5baf99b97350f6add430bc0830d
vanessa082 <wahvanessa22@gmail.com>
2024-05-22 14:13:46 +0100

    creating a trnsaform streassm and passing the transform option instead of suppling read and write options

--- transform.js
+++ transform.js
@@ -1,11 +1,39 @@
 'use strict'
-const { createGzip } = require('zlib')
-const transform = createGzip()
+// const { createGzip } = require('zlib')
+// const transform = createGzip()
 
-transform.on('data', (data) => {
-  console.log('got gzip data ', data.toString('base64'))
+// transform.on('data', (data) => {
+//   console.log('got gzip data ', data.toString('base64'))
+// })
+// transform.write('first')
+// setTimeout(() => {
+//   transform.end('second')
+// }, 500)
+
+const {Transform} = require('stream')
+const {scrypt} = require('crypto')
+const createTransformStream = () =>{
+  return new Transform({
+    decodeStrings: false,
+    encoding: 'hex',
+    transform(chunk, enc, next){
+      scrypt(chunk, 'a-salt',32,(err, key) => {
+        if(err){
+          next(err)
+          return
+        }
+        next(null, key)
+      })
+    }
+  })
+}
+
+const transform = createTransformStream()
+transform.on('data',(data) => {
+  console.log('got data :',data)
 })
-transform.write('first')
-setTimeout(() => {
-  transform.end('second')
-}, 500)
+
+transform.write('A\n')
+transform.write('B\n')
+transform.write('C\n')
+transform.end('nothing more to write')
