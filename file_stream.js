9f972bd01f78ef108fc011a395eda736239bcb88
vanessa082 <wahvanessa22@gmail.com>
2024-05-22 21:06:25 +0100

    read dir using the sync the promise and the async

--- file_stream.js
+++ file_stream.js
@@ -15,29 +15,57 @@
 //   }
 // )
 
-const {pipeline} = require('stream')
-const {join} = require('path')
-const { createReadStream, createWriteStream} = require('fs')
-const {Transform} = require('stream')
+// const {pipeline} = require('stream')
+// const {join} = require('path')
+// const { createReadStream, createWriteStream} = require('fs')
+// const {Transform} = require('stream')
 
-const createUppercaseStream = () => {
-  return new Transform({
-    transform(chunk,enc,next){
-      const uppercased = chunk.toString().toUpperCase()
-      next(null, uppercased)
-    }
-  })
+// const createUppercaseStream = () => {
+//   return new Transform({
+//     transform(chunk,enc,next){
+//       const uppercased = chunk.toString().toUpperCase()
+//       next(null, uppercased)
+//     }
+//   })
+// }
+
+// pipeline(
+//     createReadStream(__filename),
+//     createUppercaseStream(),
+//     createWriteStream(join(__dirname, 'out.txt')),
+//     (err) => {
+//       if(err){
+//         console.error(err)
+//         return
+//       }
+//       console.log('finished writing')
+//     }
+//   )
+
+// reading directories
+
+const { readdirSync, readdir } = require('fs')
+const { readdir: readdirProm } = require('fs/promises')
+
+try {
+  console.log('sync', readdirSync(__dirname))
+} catch (err) {
+  console.error(err)
 }
 
-pipeline(
-    createReadStream(__filename),
-    createUppercaseStream(),
-    createWriteStream(join(__dirname, 'out.txt')),
-    (err) => {
-      if(err){
-        console.error(err)
-        return
-      }
-      console.log('finished writing')
-    }
-  )
+readdir(__dirname, (err, files) => {
+  if (err) {
+    console.error(err)
+    return
+  }
+  console.log('callback', files)
+})
+
+async function run() {
+  const files = await readdirProm(__dirname)
+  console.log('promise', files)
+}
+
+run().catch((err) => {
+  console.error(err)
+})
